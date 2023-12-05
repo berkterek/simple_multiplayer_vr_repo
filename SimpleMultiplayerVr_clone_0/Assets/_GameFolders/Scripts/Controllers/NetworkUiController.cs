6753b92bd1cbf50eeae8dbefb2cc1e12e03c28ca
berkterek <42406263+berkterek@users.noreply.github.com>
2023-12-05 16:59:26 +0300

    feat: relay connection added

--- SimpleMultiplayerVr_clone_0/Assets/_GameFolders/Scripts/Controllers/NetworkUiController.cs
+++ SimpleMultiplayerVr_clone_0/Assets/_GameFolders/Scripts/Controllers/NetworkUiController.cs
@@ -1,3 +1,4 @@
+using SimpleMultiplayerVr.Managers;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,22 +7,23 @@ namespace SimpleMultiplayerVr.Controllers
 {
     public class NetworkUiController : MonoBehaviour
     {
+        [SerializeField] string _joinCode = "asdfa";
         [SerializeField] Button _hostButton;
         [SerializeField] Button _clientButton;
         [SerializeField] Button _serverButton;
 
         void OnEnable()
         {
-            _hostButton.onClick.AddListener(() => NetworkManager.Singleton.StartHost());
-            _clientButton.onClick.AddListener(() => NetworkManager.Singleton.StartClient());
-            _serverButton.onClick.AddListener(() => NetworkManager.Singleton.StartServer());
+            _hostButton.onClick.AddListener(() => RelayManager.Instance.CreateRelayGame(4));
+            _clientButton.onClick.AddListener(() => RelayManager.Instance.JoinRelayGame(_joinCode));
+            //_serverButton.onClick.AddListener(() => NetworkManager.Singleton.StartServer());
         }
 
         void OnDisable()
         {
             _hostButton.onClick.RemoveListener(() => NetworkManager.Singleton.StartHost());
             _clientButton.onClick.RemoveListener(() => NetworkManager.Singleton.StartClient());
-            _serverButton.onClick.RemoveListener(() => NetworkManager.Singleton.StartServer());
+            //_serverButton.onClick.RemoveListener(() => NetworkManager.Singleton.StartServer());
         }
     }
 }
---
+++ SimpleMultiplayerVr_clone_0/Assets/_GameFolders/Scripts/Managers/RelayManager.cs
@@ -0,0 +1,80 @@
+using System;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using UnityEngine;
+
+namespace SimpleMultiplayerVr.Managers
+{
+    public class RelayManager : MonoBehaviour
+    {
+        [SerializeField] UnityTransport _unityTransport;
+
+        public static RelayManager Instance { get; private set; }
+
+        void OnValidate()
+        {
+            if (_unityTransport == null) _unityTransport = GetComponent<UnityTransport>();
+        }
+
+        void Awake()
+        {
+            Instance = this;
+        }
+
+        public void CreateRelayGame(int maxPlayer)
+        {
+            TryCatchMethod(async () =>
+            {
+                Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxPlayer);
+                string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+                Debug.Log("The join code => " + joinCode);
+
+                _unityTransport.SetHostRelayData
+                (
+                    allocation.RelayServer.IpV4,
+                    (ushort)allocation.RelayServer.Port,
+                    allocation.AllocationIdBytes,
+                    allocation.Key,
+                    allocation.ConnectionData
+                );
+
+                NetworkManager.Singleton.StartHost();
+            });
+        }
+
+        public void JoinRelayGame(string joinCode)
+        {
+            TryCatchMethod(async () =>
+            {
+                JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+                _unityTransport.SetClientRelayData
+                (
+                    joinAllocation.RelayServer.IpV4,
+                    (ushort)joinAllocation.RelayServer.Port,
+                    joinAllocation.AllocationIdBytes,
+                    joinAllocation.Key,
+                    joinAllocation.ConnectionData,
+                    joinAllocation.HostConnectionData
+                );
+                
+                NetworkManager.Singleton.StartClient();
+            });
+        }
+
+        private void TryCatchMethod(System.Action insideTryCallback, System.Action insideCatchCallback = null)
+        {
+            try
+            {
+                insideTryCallback?.Invoke();
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError(e);
+                Debug.LogError(e.Message);
+                insideCatchCallback?.Invoke();
+            }
+        }
+    }
+}
